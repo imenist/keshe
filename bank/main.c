a16040131b92c50b74149ca185c1cc32503c7ead
imenist <243260197@qq.com>
2019-12-07 23:56:34 +0800

    12.7-2356win7

--- bank/main.c
+++ bank/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-
+#define MAX_SIZE 100;
 #define TRUE 1;
 #define FALSE 0;
 #define ERROR 0;
@@ -17,6 +17,13 @@ typedef struct custom{
     int ID;          //客户序号
     //struct custom *next;
 }*qcustom,custom;
+
+//顾客链栈节点
+typedef struct custNode{
+   //struct custom data[MAX_SIZE];
+   int data[50];
+   int top;
+}custNode;
 
 //事件类型
 typedef struct event{
@@ -55,9 +62,10 @@ custom *leaveCustom = NULL;          //马上离开的客户
 custom *tempCustom = NULL;           //临时的客户
 int total = 10000;                   //初始化银行总金额
 int closeTime = 600;                 //银行营业时间
-int intervalTime;                    //两个事件之间间隔时间
-int dealTimeMax;                     //客户最大交易时间
-int dealMoneyMax;                    //客户最大交易额
+int intervalTime = 15;                    //两个事件之间间隔时间
+int dealTimeMax = 15;                     //客户最大交易时间
+int dealMoneyMax = 100;                    //客户最大交易额
+//custNode *cn = NULL;                  //顾客链栈节点
 LinkList eventList = NULL;           //事件链表
 Queue *handleQueue = NULL;           //处理队列1
 Queue *waitQueue = NULL;             //等待队列2
@@ -70,8 +78,10 @@ int handleTime;                      //可以处理业务的时间
 //初始化链表
 LinkList initList(){
      LinkList L = (LinkList*)malloc(sizeof(List));
-     if(L == NULL)
+     if(L == NULL){
+        printf("Init failed");
         return NULL;
+     }
      L->head = NULL;     //为头指针开拓空间
      L->tail = NULL;
      printf("Init success\n");
@@ -80,8 +90,10 @@ LinkList initList(){
 
 //链表插入
 Status insertList(event e,LinkList L){
-     if(L == NULL)
+     if(L == NULL){
+        printf("插入失败");
         return FALSE;
+     }
      if(L->head == NULL){
         L->head = (Link)malloc(sizeof(LNode));
         L->head->data = e;
@@ -144,8 +156,10 @@ Status cleanLink(LinkList L){
 //初始化队列
 Queue* initQuene(){
    Queue *q = (Queue*)malloc(sizeof(Queue));
-   if(q == NULL)
-     return NULL;
+   if(q == NULL){
+    printf("队列初始化失败");
+    return NULL;
+   }
    q->front = NULL;
    q->rear = NULL;
    q->length = 0;
@@ -168,6 +182,7 @@ Status enQueue(Queue *q,custom c){
      q->rear->next = p;
      q->rear = p;
      q->length++;
+     printf("%d混蛋",c.ID);
    }
 }
 
@@ -188,13 +203,24 @@ Status deQueue(Queue *q,custom *c){
 }
 
 
-//队列判空
-Status emptyQueue(Queue q){
-    if(q.rear == q.front)
-        return TRUE;
-    return FALSE;
+//队列遍历
+Status traverseQueue(Queue *q){
+    if(q == NULL){
+        printf("空队列");
+        return ERROR;
+    }
+    QNode *p;
+    custom *c;
+    p = q->front;
+    while(q->rear != p){
+        *c = p->data;
+        q->front = p->next;
+        printf("123");
+        p = q->front;
+        printf("%d客户，有%d钱",c->ID,c->money);
+    }
+    return TRUE;
 }
-
 
 //队列销毁
 Status destroyQueue(Queue *q){
@@ -214,7 +240,7 @@ Status destroyQueue(Queue *q){
 
 //返回队头元素
 Status popQueue(Queue *q,custom *c){
-   if(emptyQueue(*q))
+   if(q->front == NULL)
      return FALSE;
    *c = q->front->data;
    return TRUE;
@@ -234,12 +260,23 @@ Status insertQueue(Queue *q,Queue *p){
    return TRUE;
 }
 
+/*
+//顾客链栈入栈
+Status custPush(custom *s){
+   if(cn->top == MAX_SIZE){
+    printf("接收顾客已满，请明天再来");
+    return ERROR;
+   }
+   cn->data[cn->top] = s;
+   cn->top++;
+   return TRUE;
+}
+*/
+
 //随机生成客户信息
-custom random_custom(){
+custom* random_custom(){
    custom *c;
    c = (qcustom)malloc(sizeof(custom));
-   if(c == NULL)
-    return ERROR;
    c->dealTime = (rand() % dealTimeMax) + 1;
    c->money = (rand() % 2*dealMoneyMax) - dealMoneyMax;
    c->ID = customID;
@@ -248,19 +285,21 @@ custom random_custom(){
 }
 
 //到达客户时插入事件链表，并返回客户以及获得下一个客户到达时间
-custom* arrive_event(int currentTime){
+custom* arrive_event(){
    if(currentTime == nextCustomTime){
       nextCustomTime = nextCustomTime + (rand() % intervalTime) + 1;
-      printf("当前时间： %d",&currentTime);
+      printf("当前时间： %d,",currentTime);
       custom *c;
       c = random_custom();
       event *e;                //创建事件节点
       e = (event*)malloc(sizeof(event));
-      int customID = c->id;
+      int customID = c->ID;
       e->id = customID;
       e->time = currentTime;
       e->type = 1;
       insertList(*e,eventList);
+      printf("欢迎第%d位客户,他有%d这么多钱\n",customID,c->money);
+      printf("我们银行还有%d块钱\n",total);
       return c;
    }else{
       custom *c;
@@ -271,16 +310,14 @@ custom* arrive_event(int currentTime){
 }
 
 //客户离开时插入链表
-Status leave_custom(custom c){
-    if(c == NULL)
-        return ERROR;
+Status leave_custom(custom *c){
     int customID = c->ID;
-    event *e = NULL;
-    e = (event*)malloc(sizeof(event));
+    event *e = (event*)malloc(sizeof(event));
     e->id = customID;
     e->time = currentTime;
     e->type = 0;
     insertList(*e,eventList);
+    printf("请%d位顾客慢走\n",customID);
     return TRUE;
 }
 
@@ -293,10 +330,11 @@ Status handle_custom(custom *c){
         handleTime = currentTime + c->dealTime;
         leave_custom(c);
      }else{
-         enQueue(waitQueue,c);
+         printf("请%d稍等,你的钱%d太多了\n",c->ID,c->money);
+         enQueue(waitQueue,*c);
      }
    }else{
-     enQueue(handleQueue,c);
+     enQueue(handleQueue,*c);
    }
    return TRUE;
 }
@@ -309,10 +347,13 @@ Status wait_queue(){
 
 
 int main(void){
+    //cn->top = 0;     //初始化链栈
     dealCustom = (custom*)malloc(sizeof(custom));
     leaveCustom = (custom*)malloc(sizeof(custom));
-    handleQueue = (Queue*)malloc(sizeof(Queue));
-    waitQueue = (Queue*)malloc(sizeof(Queue));
+    handleQueue = initQuene();
+    waitQueue = initQuene();
+    eventList = initList();
+    /*
     printf("\t\t\t********银行业务模拟系统********\n");
     printf("请输入两事情间隔时间:");
     scanf("%d",&intervalTime);
@@ -320,14 +361,26 @@ int main(void){
     scanf("%d",&dealTimeMax);
     printf("请输入客户最大交易额:");
     scanf("%d",&dealMoneyMax);
-    for(int i = 0;i < closeTime;i++){
+    */
+    for(currentTime = 0;currentTime < closeTime;currentTime++){
         tempCustom = (custom*)malloc(sizeof(custom));
-        tempCustom = arrive_event(i);
+        tempCustom = arrive_event();
         if(tempCustom->ID == -1)
             free(tempCustom);
-        else
-
+        else{
+            handle_custom(tempCustom);
+        }
     }
+    /*
+    dealCustom = arrive_event();
+    leaveCustom = arrive_event();
+    handle_custom(dealCustom);
+    handle_custom(leaveCustom);
+    */
+    //linkTraverse(eventList);
+    int i = 50;
+    i = traverseQueue(waitQueue);
+    printf("%d",i);
 }
 
 
